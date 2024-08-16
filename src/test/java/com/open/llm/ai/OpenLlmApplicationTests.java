187b850aea7c8f8e8e793430aacc659f3468f400
Vishwas <im.vishwas.03@gmail.com>
2024-08-17 03:11:14 +0530

    Initialized

---
+++ src/test/java/com/open/llm/ai/OpenLlmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.open.llm.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenLlmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
